c992522f269e910ab0c8d89a8a5564f8515899af
VarakronVi <130995880+VarakronVi@users.noreply.github.com>
2026-06-21 22:51:12 +0700

    fix: enable SVG images + add carousel to story page

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true,
+  },
   async rewrites() {
     return [
       {
